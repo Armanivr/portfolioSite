b7837a6b57fcd460cb5fe4bf6728a9a3c9de2349
Armanivr <armanivrijswijk@gmail.com>
2024-09-24 10:04:53 +0200

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,12 @@
+<div class="footerContainer">
+    <div class="contactBox">
+        <h2>Contact</h2>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+    </div>
+    <div class="linkBox">
+        <h2>Links</h2>
+    </div>
+</div>
